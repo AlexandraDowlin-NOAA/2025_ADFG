60fdc80cfe3064d3f2a5c1eaec711a878bbac43f
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-11-18 16:07:03 -0800

    move around data loading code

--- load_data.R
+++ load_data.R
@@ -9,7 +9,7 @@ locations <- c(
   "RACEBASE.CRUISE",
   "RACEBASE.CATCH",
   "RACEBASE.SPECIES",
-  
+
   ## Race Data tables
   "RACE_DATA.RACE_SPECIES_CODES",
   # "RACE_DATA.CRUISES",
@@ -36,6 +36,8 @@ for (i in 1:length(locations)) {
 
 ## Get RACEBASE data -----------------------------------------------
 # This local folder contains csv files of all the  tables. reads downloaded tables into R environment
+# These are haul0, catch0, species0, etc.
+
 a <- list.files(
   path = here::here("data", "oracle"),
   pattern = "\\.csv"
@@ -71,16 +73,77 @@ specimens_total <- read.csv("assets/adfg_report/goa_202501_adfg_specimens_total.
 
 # summary tables -----------------------------------------------------------
 # Load tables made in 'data_analysis.R'
-catch_summary_3nm <- readr::read_csv(here::here("output", tolower(region_abbr), 
-                                                paste0(tolower(region_abbr), "_", cruise_id,"_adfg_catch_state.csv")))
-catch_summary <- readr::read_csv(here::here("output", tolower(region_abbr), 
-                                                paste0(tolower(region_abbr), "_", cruise_id,"_adfg_catch_total.csv")))
+catch_summary_3nm <- readr::read_csv(here::here(
+  "output", tolower(region_abbr),
+  paste0(tolower(region_abbr), "_", cruise_id, "_adfg_catch_state.csv")
+))
+catch_summary <- readr::read_csv(here::here(
+  "output", tolower(region_abbr),
+  paste0(tolower(region_abbr), "_", cruise_id, "_adfg_catch_total.csv")
+))
 
-age_count_3nm <- readr::read_csv(here::here("output", tolower(region_abbr), 
-                                            paste0(tolower(region_abbr), "_", cruise_id,"_adfg_specimens_state.csv")))
-age_count <- readr::read_csv(here::here("output", tolower(region_abbr), 
-                                            paste0(tolower(region_abbr), "_", cruise_id,"_adfg_specimens_total.csv")))
+age_count_3nm <- readr::read_csv(here::here(
+  "output", tolower(region_abbr),
+  paste0(tolower(region_abbr), "_", cruise_id, "_adfg_specimens_state.csv")
+))
+age_count <- readr::read_csv(here::here(
+  "output", tolower(region_abbr),
+  paste0(tolower(region_abbr), "_", cruise_id, "_adfg_specimens_total.csv")
+))
 
+
+# Voucher counts ----------------------------------------------------------
+# counts of vouchers
+voucher_count_3nm <- catch0 %>%
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, stationid, stratum, abundance_haul)) %>%
+  # dplyr::filter(abundance_haul == "Y") %>%
+  # dplyr::inner_join(stations_3nm0 %>%
+  #                     dplyr::select(stationid, stratum) %>%
+  #                     dplyr::distinct()) %>%
+  dplyr::filter(region == SRVY &
+    stationid %in% stations_3nm0$stationid &
+    cruise == cruise1 &
+    !is.na(voucher)) %>%
+  dplyr::group_by(species_code) %>%
+  dplyr::summarise(count = n()) %>%
+  dplyr::mutate(comment = "Voucher")
+
+# combine and stack vouchers and age samples
+voucher_3nm <- dplyr::bind_rows(voucher_count_3nm, specimens_state) %>%
+  dplyr::left_join(species0) %>%
+  dplyr::select(common_name, species_name, count, comment)
+
+
+
+# voucher summary  -------------------------------------------------------------
+# counts of vouchers for all survey
+voucher_count <- catch0 %>%
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) %>%
+  # dplyr::filter(abundance_haul == "Y") %>%
+  dplyr::filter(region == SRVY &
+    cruise == cruise1 &
+    !is.na(voucher)) %>%
+  dplyr::group_by(species_code) %>%
+  dplyr::summarise(count = n()) %>%
+  dplyr::mutate(comment = "Voucher")
+
+# find out how to put together the new specimen table with the above voucher
+voucher_all <- dplyr::bind_rows(voucher_count, specimens_total) %>%
+  dplyr::left_join(species0) %>%
+  dplyr::select(common_name, species_name, count, comment)
+
+# voucher_count <- catch0 %>%
+#   dplyr::filter(region == SRVY &
+#                   cruise == cruise1 &
+#                   !is.na(voucher)) %>%
+#   dplyr::group_by(species_code) %>%
+#   dplyr::summarise(count = sum(number_fish, na.rm=T)) %>%
+#   dplyr::mutate(comment  = "Voucher")
+
+# combine and stack vouchers and age samples
+voucher_all <- dplyr::bind_rows(voucher_count, age_count) %>%
+  dplyr::left_join(species0) %>%
+  dplyr::select(common_name, species_name, count, comment)
 
 
 # Old catch_summary and age_count tables ----------------------------------
@@ -142,56 +205,3 @@ age_count <- readr::read_csv(here::here("output", tolower(region_abbr),
 #   dplyr::group_by(species_code) %>%
 #   dplyr::summarise(count = n()) %>%
 #   dplyr::mutate(comment = "Age Sample")
-
-# Voucher counts ----------------------------------------------------------
-# counts of vouchers
-voucher_count_3nm <- catch0 %>%
-  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, stationid, stratum, abundance_haul)) %>%
-  # dplyr::filter(abundance_haul == "Y") %>%
-  # dplyr::inner_join(stations_3nm0 %>%
-  #                     dplyr::select(stationid, stratum) %>%
-  #                     dplyr::distinct()) %>%
-  dplyr::filter(region == SRVY &
-                  stationid %in% stations_3nm0$stationid &
-                  cruise == cruise1 &
-                  !is.na(voucher)) %>%
-  dplyr::group_by(species_code) %>%
-  dplyr::summarise(count = n()) %>%
-  dplyr::mutate(comment = "Voucher")
-
-# combine and stack vouchers and age samples
-voucher_3nm <- dplyr::bind_rows(voucher_count_3nm, specimens_state) %>%
-  dplyr::left_join(species0) %>%
-  dplyr::select(common_name, species_name, count, comment)
-
-
-
-# voucher summary  -------------------------------------------------------------
-# counts of vouchers for all survey
-voucher_count <- catch0 %>%
-  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) %>%
-  # dplyr::filter(abundance_haul == "Y") %>%
-  dplyr::filter(region == SRVY &
-                  cruise == cruise1 &
-                  !is.na(voucher)) %>%
-  dplyr::group_by(species_code) %>%
-  dplyr::summarise(count = n()) %>%
-  dplyr::mutate(comment = "Voucher")
-
-# find out how to put together the new specimen table with the above voucher
-voucher_all <- dplyr::bind_rows(voucher_count, specimens_total) %>%
-  dplyr::left_join(species0) %>%
-  dplyr::select(common_name, species_name, count, comment)
-
-# voucher_count <- catch0 %>%
-#   dplyr::filter(region == SRVY &
-#                   cruise == cruise1 &
-#                   !is.na(voucher)) %>%
-#   dplyr::group_by(species_code) %>%
-#   dplyr::summarise(count = sum(number_fish, na.rm=T)) %>%
-#   dplyr::mutate(comment  = "Voucher")
-
-# combine and stack vouchers and age samples
-voucher_all <- dplyr::bind_rows(voucher_count, age_count) %>%
-  dplyr::left_join(species0) %>%
-  dplyr::select(common_name, species_name, count, comment)
