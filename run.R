222d437b5a43bc4c739bef4d184313426f4c8ec8
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-11-18 16:40:43 -0800

    add proposed order to run scripts in into run.R

--- run.R
+++ run.R
@@ -24,3 +24,16 @@ dir.create(path = dir_out)
 rmarkdown::render(paste0("./ADFG_REPORT.Rmd"),
                   output_dir = dir_out,
                   output_file = paste0(maxyr,"_ADFG_REPORT.docx"))
+
+
+#  MCS proposed order of files -------------------------------------------------
+# Support scripts --------------------------------------------------------------
+refresh_tables <- FALSE #set to true if downloading new data
+if(refresh_tables){
+  source('./1.make_layers_goa.R')  # code from navmaps
+  source('./adfg_report_tables_goa_ai.R') #code from navmaps and Sean
+}
+
+source('./load_data.R')
+source('./data_analysis.R')
+
