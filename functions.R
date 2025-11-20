6ad72492635394481a6aaa57128346a79a749715
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-11-20 14:57:16 -0800

    update functions.R

--- functions.R
+++ functions.R
@@ -1,3 +1,12 @@
+# Install if needed
+
+if (!"akgfmaps" %in% installed.packages()) {
+  install_github("afsc-gap-products/akgfmaps", build_vignettes = TRUE)
+}
+if (!"navmaps" %in% installed.packages()) {
+  install_github("afsc-gap-products/navmaps", auth_token = gh::gh_token())
+}
+
 # Load packages
 library(knitr)
 library(ggplot2)
@@ -11,13 +20,7 @@ library(plyr)
 library(ggplot2)
 library(getPass)
 library(navmaps) # afsc-gap-products/navmaps (v 1.1.10)
-library(akgfmaps) #afsc-gap-products/akgfmaps (v 4.0.3)
+library(akgfmaps) # afsc-gap-products/akgfmaps (v 4.0.3)
 library(tidyr)
 library(here)
 library(remotes)
-
-install_github("afsc-gap-products/akgfmaps", build_vignettes = TRUE)
-
-library(remotes)
-
-install_github("afsc-gap-products/navmaps", auth_token = gh::gh_token())
