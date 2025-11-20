076c09553b46f465ab298448e02ad29867c8b967
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-11-20 14:48:14 -0800

    move all analysis to data analysis script

--- data_analysis.R
+++ data_analysis.R
@@ -22,6 +22,8 @@ vessel2 <- "FV Alaska Provider"
 
 cruise_id <- 202501
 
+data_finalized <- "13 September, 2023"
+
 # successful biomass tows 526sql
 s_stations <- haul0 %>%
   filter(abundance_haul == "Y") %>%
@@ -167,6 +169,19 @@ voucher_all_count <- voucher_all %>%
   summarize(total = sum(N_RECORDS_TOTAL)) %>%
   as.numeric()
 
+voucher_fish_taxa <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+                  cruise == cruise1 &
+                  !is.na(voucher)) |>
+  dplyr::group_by(species_code) |>
+  dplyr::summarise(count = n()) |>
+  dplyr::left_join(species0) |>
+  dplyr::mutate(taxon = dplyr::case_when(
+    species_code <= 31550 ~ "fish",
+    species_code >= 40001 ~ "invert"
+  ))
+
 # total count of taxa otolith were collected from in all survey
 oto_taxa <- voucher_all %>%
   filter(SAMPLE_TYPE == "Otoliths") %>%
@@ -191,9 +206,6 @@ oto_3nm_taxa <- voucher_age_3nm %>%
   nrow()
 
 
-data_finalized <- "13 September, 2023"
-
-
 # Total catch
 total_wt_all_catch <- catch_total %>%
   summarize(total = sum(TOTAL_WEIGHT_KG )) %>%
@@ -206,3 +218,188 @@ total_wt_3nm_catch <- catch_state %>%
   summarize(total = sum(TOTAL_WEIGHT_KG_STATE )) %>%
   as.numeric()
 total_wt_3nm_80 <- total_wt_3nm_catch * 0.8
+
+# Voucher summary 3nm ----------------------------------------------------------
+# MCS: need to confirm! does this look like a reasonable # of vouchers in state waters?
+voucher_count_3nm <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, stationid, stratum, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+    cruise == cruise1 &
+    !is.na(voucher)) |>
+  dplyr::right_join(state_hauls, by = c("cruise" = "CRUISE", "vessel" = "VESSEL", "haul" = "HAUL")) |> # keep only stuff in state hauls
+  dplyr::filter(!is.na(species_code)) |> # remove entries that aren't in state_hauls
+  dplyr::group_by(species_code) |>
+  dplyr::summarise(count = n()) |>
+  dplyr::left_join(species0) |> # add common names and species names
+  dplyr::mutate(SAMPLE_TYPE = "Voucher") |>
+  dplyr::rename(
+    SPECIES_CODE = "species_code",
+    SPECIES_NAME = "species_name",
+    COMMON_NAME = "common_name",
+    N_RECORDS_STATE = "count"
+  )
+
+
+# combine and stack vouchers and age samples
+voucher_age_3nm <- dplyr::bind_rows(voucher_count_3nm, age_count_3nm) %>%
+  dplyr::select(COMMON_NAME, SPECIES_NAME, N_RECORDS_STATE, SAMPLE_TYPE)
+
+
+# voucher summary all survey  --------------------------------------------------
+# counts of vouchers for all survey
+voucher_count <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+    cruise == cruise1 &
+    !is.na(voucher)) |>
+  dplyr::group_by(species_code) |>
+  dplyr::summarise(count = n()) |>
+  dplyr::left_join(species0) |>
+  dplyr::mutate(SAMPLE_TYPE = "Voucher") |>
+  dplyr::rename(
+    SPECIES_CODE = species_code,
+    COMMON_NAME = common_name,
+    SPECIES_NAME = species_name,
+    N_RECORDS_TOTAL = count
+  )
+
+# find out how to put together the new specimen table with the above voucher
+voucher_age_all <- dplyr::bind_rows(voucher_count, age_count) |>
+  dplyr::select(COMMON_NAME, SPECIES_NAME, N_RECORDS_TOTAL, SAMPLE_TYPE)
+
+voucher_taxa <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+                  cruise == cruise1 &
+                  !is.na(voucher)) |>
+  dplyr::group_by(species_code) |>
+  dplyr::mutate(taxon = dplyr::case_when(
+    species_code <= 31550 ~ "fish",
+    species_code >= 40001 ~ "invert"
+  )) 
+
+voucher_fish_taxa_count <- voucher_taxa |>
+  dplyr::filter(taxon == "fish") |>
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+voucher_invert_taxa_count <- voucher_taxa |>
+  dplyr::filter(taxon == "invert") |>
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+voucher_fish_taxa_count_3nm <- voucher_taxa |>
+  dplyr::filter(taxon == "fish") |>
+  right_join(state_hauls, by = c("vessel" = "VESSEL",
+                                 "haul" = "HAUL",
+                                 "cruise" = "CRUISE")) |>
+  dplyr::filter(!is.na(hauljoin)) |>  # these are hauls that are outside 3nm
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+voucher_invert_taxa_count_3nm <- voucher_taxa |>
+  dplyr::filter(taxon == "invert") |>
+  right_join(state_hauls, by = c("vessel" = "VESSEL",
+                                 "haul" = "HAUL",
+                                 "cruise" = "CRUISE")) |>
+  dplyr::filter(!is.na(hauljoin)) |>  # these are hauls that are outside 3nm
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+
+
+
+# Voucher summary 3nm ----------------------------------------------------------
+# MCS: need to confirm! does this look like a reasonable # of vouchers in state waters?
+voucher_count_3nm <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, stationid, stratum, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+    cruise == cruise1 &
+    !is.na(voucher)) |>
+  dplyr::right_join(state_hauls, by = c("cruise" = "CRUISE", "vessel" = "VESSEL", "haul" = "HAUL")) |> # keep only stuff in state hauls
+  dplyr::filter(!is.na(species_code)) |> # remove entries that aren't in state_hauls
+  dplyr::group_by(species_code) |>
+  dplyr::summarise(count = n()) |>
+  dplyr::left_join(species0) |> # add common names and species names
+  dplyr::mutate(SAMPLE_TYPE = "Voucher") |>
+  dplyr::rename(
+    SPECIES_CODE = "species_code",
+    SPECIES_NAME = "species_name",
+    COMMON_NAME = "common_name",
+    N_RECORDS_STATE = "count"
+  )
+
+
+# combine and stack vouchers and age samples
+voucher_age_3nm <- dplyr::bind_rows(voucher_count_3nm, age_count_3nm) %>%
+  dplyr::select(COMMON_NAME, SPECIES_NAME, N_RECORDS_STATE, SAMPLE_TYPE)
+
+
+# voucher summary all survey  --------------------------------------------------
+# counts of vouchers for all survey
+voucher_count <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+    cruise == cruise1 &
+    !is.na(voucher)) |>
+  dplyr::group_by(species_code) |>
+  dplyr::summarise(count = n()) |>
+  dplyr::left_join(species0) |>
+  dplyr::mutate(SAMPLE_TYPE = "Voucher") |>
+  dplyr::rename(
+    SPECIES_CODE = species_code,
+    COMMON_NAME = common_name,
+    SPECIES_NAME = species_name,
+    N_RECORDS_TOTAL = count
+  )
+
+# find out how to put together the new specimen table with the above voucher
+voucher_age_all <- dplyr::bind_rows(voucher_count, age_count) |>
+  dplyr::select(COMMON_NAME, SPECIES_NAME, N_RECORDS_TOTAL, SAMPLE_TYPE)
+
+voucher_taxa <- catch0 |>
+  dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) |>
+  dplyr::filter(region == SRVY &
+                  cruise == cruise1 &
+                  !is.na(voucher)) |>
+  dplyr::group_by(species_code) |>
+  dplyr::mutate(taxon = dplyr::case_when(
+    species_code <= 31550 ~ "fish",
+    species_code >= 40001 ~ "invert"
+  )) 
+
+voucher_fish_taxa_count <- voucher_taxa |>
+  dplyr::filter(taxon == "fish") |>
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+voucher_invert_taxa_count <- voucher_taxa |>
+  dplyr::filter(taxon == "invert") |>
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+voucher_fish_taxa_count_3nm <- voucher_taxa |>
+  dplyr::filter(taxon == "fish") |>
+  right_join(state_hauls, by = c("vessel" = "VESSEL",
+                                 "haul" = "HAUL",
+                                 "cruise" = "CRUISE")) |>
+  dplyr::filter(!is.na(hauljoin)) |>  # these are hauls that are outside 3nm
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
+
+voucher_invert_taxa_count_3nm <- voucher_taxa |>
+  dplyr::filter(taxon == "invert") |>
+  right_join(state_hauls, by = c("vessel" = "VESSEL",
+                                 "haul" = "HAUL",
+                                 "cruise" = "CRUISE")) |>
+  dplyr::filter(!is.na(hauljoin)) |>  # these are hauls that are outside 3nm
+  distinct(species_code) |>
+  nrow() |>
+  as.numeric()
