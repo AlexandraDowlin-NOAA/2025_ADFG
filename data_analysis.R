e16bfa8f70af126540145ab706f7e7f181dfb22d
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-11-24 15:27:33 -0800

    fix voucher tables

--- data_analysis.R
+++ data_analysis.R
@@ -165,8 +165,8 @@ inverts_all_wt <- catch_total |>
 voucher_count_3nm <- catch0 |>
   dplyr::left_join(haul0 |> dplyr::select(hauljoin, stationid, stratum, abundance_haul)) |>
   dplyr::filter(region == SRVY &
-                  cruise == cruise1 #&
-                #  !is.na(voucher)
+                  cruise == cruise1 &
+                  !is.na(voucher)
                 ) |>
   dplyr::right_join(state_hauls, by = c("cruise" = "CRUISE", "vessel" = "VESSEL", "haul" = "HAUL")) |> # keep only stuff in state hauls
   dplyr::filter(!is.na(species_code)) |> # remove entries that aren't in state_hauls
@@ -193,11 +193,11 @@ voucher_count <- catch0 |>
   dplyr::left_join(haul0 %>% dplyr::select(hauljoin, abundance_haul)) |>
   dplyr::filter(region == SRVY &
                   cruise == cruise1 &
-                  !is.na(voucher)) |>
+                  !is.na(voucher)) |> # pull out only voucher entries
   dplyr::group_by(species_code) |>
   dplyr::summarise(count = n()) |>
   dplyr::left_join(species0) |>
-  #dplyr::mutate(SAMPLE_TYPE = "Voucher") |>
+  dplyr::mutate(SAMPLE_TYPE = "Voucher") |> # add a column saying they are vouchers
   dplyr::rename(
     SPECIES_CODE = species_code,
     COMMON_NAME = common_name,
